2e6513f2bd066d1cda7cf1ccec52bd3753b1e44b
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-04-21 23:39:09 -0500

    :ambulance: Fixing useEffect hook

--- src/components/sectionList.js
+++ src/components/sectionList.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 //Components
 import { Title, CustomExternalLink } from './common'
 
-const SectionList = ({ id, title, items}) => (
-    <div id={id}>
-        <Title to={`${window.location.pathname}#${id}`}>{title}</Title>
-        <ul>
-            {items.map(item => (
-                <li key={item.name}><CustomExternalLink href={item.link} target="_blank">{item.name}</CustomExternalLink>: {item.description}</li>
-            ))}
-        </ul>
-    </div>
-)
+const SectionList = ({ id, title, items}) => {
+
+    const [pathname, setPathname] = useState('')
+    useEffect(() => {
+        setPathname(window.location.pathname)
+    }, [pathname])
+
+    return (
+        <div id={id}>
+            <Title to={`${pathname}#${id}`}>{title}</Title>
+            <ul>
+                {items.map(item => (
+                    <li key={item.name}><CustomExternalLink href={item.link} target="_blank">{item.name}</CustomExternalLink>: {item.description}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default SectionList
